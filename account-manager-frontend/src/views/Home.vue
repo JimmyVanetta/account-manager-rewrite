ecd1129bd096335be0c20b3c9f72c44ec0c0c5ce
JimmyVanetta <j_vanetta@zoominternet.net>
2021-11-12 16:18:04 -0500

    scaffolded store, router and api layer

--- account-manager-frontend/src/views/Home.vue
+++ account-manager-frontend/src/views/Home.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '../components/HelloWorld.vue';
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    HelloWorld,
+  },
+};
 </script>
